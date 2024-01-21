6ae3f6884c9a14dce90eb54df2ad4908dc6454a4
Orbyfied <officialglitchout@gmail.com>
2024-01-21 16:24:12 +0100

    add entrypoint

---
+++ main.py
@@ -0,0 +1,13 @@
+##########################################
+# ffphysim/spe3d
+##########################################
+
+import sys
+
+def execute_application(args: list):
+    ### Executes the application, utilizing the library to simulate stuff
+
+    pass    
+
+if __name__ == "__main__":
+    execute_application(sys.argv)
